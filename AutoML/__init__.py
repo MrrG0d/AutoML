f284dcdef5b062f441d8549cfab404ffe89ff5f2
MrrG0d <91164262+MrrG0d@users.noreply.github.com>
2023-05-21 19:24:12 +0300

    Add files via upload

---
+++ AutoML/__init__.py
@@ -0,0 +1,43 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "a75e64ff",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "__all__ = [\"ml_pipeline\", \"predictive_models\", \"data_preprocessing\"]"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": None,
+   "id": "347c6882",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
