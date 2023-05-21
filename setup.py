006c69fcdb68bbc9c71d00ce0937e4ab82a0a739
MrrG0d <91164262+MrrG0d@users.noreply.github.com>
2023-05-21 19:24:39 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,45 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": None,
+   "id": "2247c939",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "from setuptools import setup, find_packages\n",
+    "\n",
+    "setup(\n",
+    "    name='AutoML',\n",
+    "    version='0.1',\n",
+    "    packages=find_packages(),\n",
+    "    description='AutoML library',\n",
+    "    author='MrGod',\n",
+    "    author_email='sergey.alekseev.01@inbox.ru',\n",
+    "    url='https://github.com/MrrG0d/AutoML.git',\n",
+    ")"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
